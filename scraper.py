3511e51f15e239eec4aa4479a8b89c56573e91a3
Diego Sanchez <diegoas6@uci.edu>
2025-04-30 01:19:58 -0700

    Adjust filter to find more pages

--- scraper.py
+++ scraper.py
@@ -88,6 +88,9 @@ def is_valid(url):
         if date_pattern.search(query):
             return False
 
+        if re.search(r'/day/(19|20)\d{2}-\d{2}-\d{2}', path):
+            return False
+
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
